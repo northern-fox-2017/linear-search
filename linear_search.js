3b2c0b2614fd3261295660cd94ff8e49886caa0d
juliusade <juliusade15@gmail.com>
2017-11-14 19:02:45 +0700

    release 0

--- linear_search.js
+++ linear_search.js
@@ -4,7 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let i=0; i<values.length; i++) {
+    if(target == values[i]) {
+      return i
+    }
+  }
+  return -1
 }
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
@@ -18,7 +25,6 @@ console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
 }
-
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
