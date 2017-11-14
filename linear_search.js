fb773903123e737c220bc9c5998379bdf327490f
Rahmat Hidayat <email": null>
2017-11-14 19:42:48 +0700

    linear search done

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var index = -1;
+
+  for(let i = 0; i < random_numbers.length; i++) {
+    if(random_numbers[i] === target) {
+      return i;
+    }
+  }
+  return index;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +23,15 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var arrIndex = []
+
+  for(let i = 0; i < banana_arr.length; i++) {
+    if(banana_arr[i] === target) {
+      arrIndex.push(i);
+    }
+  }
+
+  return arrIndex;
 }
 
 let banana_arr = "banana".split("");
