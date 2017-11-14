980a6f536376718a9f2fdcca0e528840593187e5
yofri <yofriadiyahya@gmail.com>
2017-11-14 14:31:23 +0700

    Finished linear_search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  // write your code here
+  for (const index in values) {
+    if (values[index] === target) return index;
+  }
+
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +21,14 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  // write your code here
+  const arrayResult = [];
+  for (const index in values) {
+    if (values[index] === target) {
+      arrayResult.push(index);
+    }
+  }
+  return arrayResult;
 }
 
 let banana_arr = "banana".split("");
